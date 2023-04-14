9d530f2833672476eb94b69b1a828bcd0317c9ff
DragonDeezNoot_98 <babybangunny@gmail.com>
2023-04-14 22:46:07 +0700

    Add workflow for github

--- next.config.mjs
+++ next.config.mjs
@@ -34,5 +34,12 @@ const config = {
       },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
 }
 export default config
