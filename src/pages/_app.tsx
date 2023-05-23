24dca1a5435595be1a25eee1d376b3dbe53ff696
DragonDeezNoot_98 <babybangunny@gmail.com>
2023-05-23 12:51:17 +0700

    remove unused function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
-import { THEME } from '@/hooks/useToggleTheme'
 import { myStore } from '@/store/store'
 import '@/styles/globals.css'
 import { api } from '@/utils/api'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { type NextPage } from 'next'
 import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
-import { ThemeProvider } from 'next-themes'
 import { type AppProps } from 'next/app'
 import Head from 'next/head'
 import { type ReactElement, type ReactNode } from 'react'
@@ -29,19 +26,12 @@ const MyApp = ({ Component, pageProps: { ...pageProps } }: MyAppProps) => {
         <meta name='description' content='A spotify cloned' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <ThemeProvider
-        attribute='data-theme'
-        disableTransitionOnChange
-        defaultTheme={THEME.DARK}
-        themes={[THEME.DARK]}
-      >
-        <SessionProvider session={pageProps.session as Session}>
-          <ReduxProvider store={myStore}>
-            {getLayout(<Component {...pageProps} />)}
-            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-          </ReduxProvider>
-        </SessionProvider>
-      </ThemeProvider>
+      <SessionProvider session={pageProps.session as Session}>
+        <ReduxProvider store={myStore}>
+          {getLayout(<Component {...pageProps} />)}
+          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+        </ReduxProvider>
+      </SessionProvider>
     </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html data-theme='dracula'>
-      <Head />
+    <Html>
+      <Head data-theme='halloween' />
       <body>
         <Main />
         <NextScript />
